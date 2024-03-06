96b23ab589443a665adac51d4b637cf2da96dde0
edward-matsyshen <edward.d.matsyshen@gmail.com>
2024-03-06 13:13:25 -0800

    DOOR FINALLY WORKS

---
+++ Assets/_Course Library/Part 6 - Backrooms Game Lab-20240304T175531Z-001/doorTrigCScode.cs
@@ -0,0 +1,31 @@
+using UnityEngine.XR;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class DoorManager : MonoBehaviour // This script should be on the Door Trigger
+{
+    public Animation Door;
+    public AudioSource DoorOpenSound;
+    private bool isDoorOpen = false; // To prevent the door from being repeatedly triggered
+
+    void Update()
+    {
+        // Check if the player is close enough to the door and the door is not already open
+        if (PlayerCasting.DistanceFromTarget < 4 && !isDoorOpen)
+        {
+            // Check for A button press on the right-hand controller
+            if (InputDevices.GetDeviceAtXRNode(XRNode.RightHand).TryGetFeatureValue(CommonUsages.primaryButton, out bool isPressed) && isPressed)
+            {
+                OpenDoor();
+            }
+        }
+    }
+
+    void OpenDoor()
+    {
+        GetComponent<BoxCollider>().enabled = false; // Optionally disable the collider to prevent re-triggering
+        Door.Play(); // Play the door open animation
+        DoorOpenSound.Play(); // Play the door open sound
+        isDoorOpen = true; // Mark the door as open
+    }
+}
