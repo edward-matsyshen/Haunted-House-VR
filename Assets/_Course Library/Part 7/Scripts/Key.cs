a3992341d62c344821f26bbc594cccbd946793fe
Calvin <calvinho0302@gmail.com>
2024-03-11 19:09:52 -0700

    Minor adjustments
    Merged and readjusted code for our scene.

--- Assets/_Course Library/Part 7/Scripts/Key.cs
+++ Assets/_Course Library/Part 7/Scripts/Key.cs
@@ -8,31 +8,49 @@ public class Key : MonoBehaviour
     [Tooltip("The name of the key. This corresponds with the key on the door")] public string keyName;
 
     public GameObject HoverIcon;
+    private bool isPlayerClose = false;
+    [SerializeField] private GameObject[] HoverObjects;
 
     [Header("(Optional)")]
     [Tooltip("(Optional.)")] public AudioClip CollectAudio;
 
     public void Update()
     {
-        if (PlayerCasting.DistanceFromTarget <= 5) // Is the player in range?
+        UpdateHoverObjectsState();
+        var leftHandDevice = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
+        leftHandDevice.TryGetFeatureValue(CommonUsages.triggerButton, out bool triggerPressed);
+
+        if (triggerPressed && isPlayerClose)
         {
-            var leftHandDevice = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
-            leftHandDevice.TryGetFeatureValue(CommonUsages.triggerButton, out bool triggerPressed);
+            if (CollectAudio != null) FindObjectOfType<KeyManager>().GetComponent<AudioSource>().PlayOneShot(CollectAudio); // (Optional.)
 
-            if (triggerPressed)
-            {
-                if (CollectAudio != null) FindObjectOfType<KeyManager>().GetComponent<AudioSource>().PlayOneShot(CollectAudio); // (Optional.)
+            FindObjectOfType<KeyManager>().keysInInventory.Add(keyName);
 
-                FindObjectOfType<KeyManager>().keysInInventory.Add(keyName);
-
-                Destroy(this.gameObject);
-            }
-
-            
+            Destroy(this.gameObject);
         }
-        
-        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // Ensure your player GameObject has a "Player" tag
+        {
+            isPlayerClose = true;
+        }
     }
 
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerClose = false;
+        }
+    }
+
+    private void UpdateHoverObjectsState()
+    {
+        foreach (GameObject hoverObject in HoverObjects)
+        {
+            hoverObject.SetActive(isPlayerClose);
+        }
+    }
 
 }
