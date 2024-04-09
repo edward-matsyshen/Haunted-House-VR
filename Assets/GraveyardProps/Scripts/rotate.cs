74c19998a6d70c8832ed7183130840d317ec4391
Edward Matsyshen <edward.d.matsyshen@gmail.com>
2024-04-08 20:50:07 -0700

    Added Asset Pack!!
    Fog, Jumpscare, and other creepy assets

---
+++ Assets/GraveyardProps/Scripts/rotate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour {
+
+    private float y = 0.0f;
+    public float rotationSpeed = 1f; // You can adjust this value in the inspector for a faster or slower rotation
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Increment y by a small value multiplied by deltaTime
+        // This will make the rotation speed consistent across different frame rates
+        // Decrease rotationSpeed or adjust the multiplier for a slower rotation
+        y += rotationSpeed * Time.deltaTime;
+        transform.localRotation = Quaternion.Euler(0, y, 0);
+    }
+}
